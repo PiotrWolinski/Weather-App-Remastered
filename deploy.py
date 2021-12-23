f871270b172f76196d1a2df31e22799a2eef8aad
PiotrWolinski <55030310+PiotrWolinski@users.noreply.github.com>
2021-12-23 18:20:56 +0100

    Completed deployment script. For now it contains all options I need. It may be expanded in the future.

--- deploy.py
+++ deploy.py
@@ -1,29 +1,62 @@
-"""Script for deploying app with given settings.
+"""Script for deploying and overviewing app.
 
-Command line arguments:
- -d - run detached
- -n - build without using cache
-
-Keyword arguments:
-argument -- description
-Return: return_description
+All CLI arguments are parsed using argparse. Add -h to get help.
 """
 
 import subprocess
 import sys
+import argparse
 
 def deploy(detached=False, no_cache=False):
-    subprocess.check_call(f"docker-compose build {'--no-cache' if no_cache else ''}", stdout=sys.stdout, stderr=subprocess.STDOUT)
-    subprocess.check_call(f"docker-compose up {'-d' if detached else ''}", stdout=sys.stdout, stderr=subprocess.STDOUT)
+    """"Build and deploy containers.
+
+    Arguments:
+    - detached - run containers in detached mode
+    - no_cache - build services without cache
+    """
+    # Remove services if any are running
+    stop_and_remove_services()
+
+    try:
+        subprocess.check_call(f"docker-compose build {'--no-cache' if no_cache else ''}", stdout=sys.stdout, stderr=subprocess.STDOUT)
+        subprocess.check_call(f"docker-compose up {'-d' if detached else ''}", stdout=sys.stdout, stderr=subprocess.STDOUT)
+    except:
+        pass
+
+def stop_and_remove_services():
+    """Stop and remove running docker containers.
+    """
+    try:
+        subprocess.check_call("docker-compose down", stdout=sys.stdout, stderr=subprocess.STDOUT)
+    except:
+        pass
+
+def show_logs():
+    """Show logs output.
+    """
+    
+    try:
+        subprocess.check_call(f"docker-compose logs", stdout=sys.stdout, stderr=subprocess.STDOUT)
+    except:
+        pass
 
 def main():
-    opts = [opt[1:].lower() for opt in sys.argv[1:] if opt.startswith("-")]
-    print(f'opts = {opts}')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--detached", help="run containers detached", action="store_true")
+    parser.add_argument("-n", "--no_cache", help="build services without cache", action="store_true")
+    parser.add_argument("-l", "--logs", help="stop and remove containers if any are running", action="store_true")
+    parser.add_argument("--down", help="stop and remove containers if any are running", action="store_true")
 
-    detached = 'd' in opts
-    no_cache = 'n' in opts
+    args = parser.parse_args()
 
-    deploy(detached=detached, no_cache=no_cache)
+    if args.logs:
+        show_logs()
+
+    if args.down:
+        stop_and_remove_services()
+
+    if not args.logs and not args.down:
+        deploy(detached=args.detached, no_cache=args.no_cache)
 
 if __name__ == '__main__':
     main()
