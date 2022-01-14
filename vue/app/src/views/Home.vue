4e06101db9d0cb83ac334d5cca3b8352cc45f8a7
PiotrWolinski <55030310+PiotrWolinski@users.noreply.github.com>
2022-01-14 19:56:54 +0100

    Started to develop frontend. Added base for makefile.

---
+++ vue/app/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-container>
+      <b-row class="align-items-center">
+        <b-col cols="5"></b-col>
+        <b-col cols="2">
+          <p>Home page</p>
+        </b-col>
+        <b-col cols="5"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {},
+};
+</script>
